2e3504ee776430295e177a04ecfcc5412f6b71ed
Truong5724 <hoangtruongnhatvu@gmail.com>
2025-05-08 22:01:35 +0700

    Truong_Update_HomePage

--- hotel_manager/src/App.js
+++ hotel_manager/src/App.js
@@ -14,44 +14,46 @@ function App() {
         <nav className="header-right">
           <a href="#about">About Us</a>
           <img src="/icons/VietnamFlag.png" alt="Vietnam Flag" className="flag" width="100" height="auto"/>
-          <button className="button">Đăng ký</button>
-          <button className="button">Đăng nhập</button>
+          <button className="button-reg">Đăng ký</button>
+          <button className="button-log">Đăng nhập</button>
         </nav>
       </header>
 
       {/* Main Content*/}
       <main className="main-content">
         <h2>Trang chủ</h2>
-        <div className=".function-grid">
-          <div className=".function-item">
-            <img src="/icons/Catalogue.png" alt="Lập danh mục thuế phòng" />
-            <p>Lập danh mục thuế phòng</p>
+        <div className="function-grid">
+  
+          <div className="function-item">
+            <img src="/icons/Catalogue.png" alt="Lập danh mục thuê phòng" />
+            <p>LẬP DANH MỤC THUÊ PHÒNG</p>
           </div>
 
-          <div className=".function-item">
-            <img src="/icons/List.png" alt="Lập phiếu thuế phòng" />
-            <p>Lập phiếu thuế phòng</p>
+          <div className="function-item">
+            <img src="/icons/List.png" alt="Lập phiếu thuê phòng" />
+            <p>LẬP PHIẾU THUÊ PHÒNG</p>
           </div>
 
-          <div className=".function-item">
+          <div className="function-item">
             <img src="/icons/Find.png" alt="Tra cứu phòng" />
-            <p>Tra cứu phòng</p>
+            <p>TRA CỨU PHÒNG</p>
           </div>
 
-          <div className=".function-item">
+          <div className="function-item">
             <img src="/icons/Receipt.png" alt="Lập hóa đơn thanh toán" />
-            <p>Lập hóa đơn thanh toán</p>
-          </div>
-          
-          <div className=".function-item">
-            <img src="/icons/Report.png" alt="Lập báo cáo tháng" />
-            <p>Lập báo cáo tháng</p>
+            <p>LẬP HÓA ĐƠN THANH TOÁN</p>
           </div>
 
-          <div className=".function-item">
-            <img src="/icons/Regulation.png" alt="Thay đổi quy định" />
-            <p>Thay đổi quy định</p>
+          <div className="function-item">
+            <img src="/icons/Report.png" alt="Lập báo cáo tháng" />
+            <p>LẬP BÁO CÁO THÁNG</p>
           </div>
+
+          <div className="function-item">
+            <img src="/icons/Regulation.png" alt="Thay đổi quy định" />
+            <p>THAY ĐỔI QUY ĐỊNH</p>
+          </div>
+
         </div>
       </main>
     </div>
