331b2cafdfc32fdfb1bce9adc6ba817370d60b2b
Truong5724 <hoangtruongnhatvu@gmail.com>
2025-05-09 09:12:31 +0700

    Truong_Update_HomePage

--- hotel_manager/src/App.js
+++ hotel_manager/src/App.js
@@ -4,55 +4,60 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/*Header*/}
+      {/* Header */}
       <header className="App-header">
-        {/* Phần bên trái của header */}
+
         <div className="header-left">
           <h1>HotelManager</h1>
         </div>
-        {/* Phần bên phải của header */}
+        
         <nav className="header-right">
-          <a href="#about">About Us</a>
-          <img src="/icons/VietnamFlag.png" alt="Vietnam Flag" className="flag" width="100" height="auto"/>
-          <button className="button-reg">Đăng ký</button>
-          <button className="button-log">Đăng nhập</button>
+          <a href="about.html">About Us</a>
+          <img src="/icons/VietnamFlag.png" alt="Vietnam Flag" className="flag"/>
+          <a href='register.html'>
+            <button className="button-reg">Đăng ký</button>
+          </a>
+          <a href='login.html'>
+            <button className="button-log">Đăng nhập</button>
+          </a>
         </nav>
+
       </header>
 
-      {/* Main Content*/}
+      {/* Main Content */}
       <main className="main-content">
         <h2>Trang chủ</h2>
         <div className="function-grid">
   
-          <div className="function-item">
+          <a href="catalogue.html" className="function-item">
             <img src="/icons/Catalogue.png" alt="Lập danh mục thuê phòng" />
             <p>LẬP DANH MỤC THUÊ PHÒNG</p>
-          </div>
+          </a>
 
-          <div className="function-item">
+          <a href="list.html" className="function-item">
             <img src="/icons/List.png" alt="Lập phiếu thuê phòng" />
             <p>LẬP PHIẾU THUÊ PHÒNG</p>
-          </div>
+          </a>
 
-          <div className="function-item">
+          <a href="find.html" className="function-item">
             <img src="/icons/Find.png" alt="Tra cứu phòng" />
             <p>TRA CỨU PHÒNG</p>
-          </div>
+          </a>
 
-          <div className="function-item">
+          <a href="receipt.html" className="function-item">
             <img src="/icons/Receipt.png" alt="Lập hóa đơn thanh toán" />
             <p>LẬP HÓA ĐƠN THANH TOÁN</p>
-          </div>
+          </a>
 
-          <div className="function-item">
+          <a href="report.html" className="function-item">
             <img src="/icons/Report.png" alt="Lập báo cáo tháng" />
             <p>LẬP BÁO CÁO THÁNG</p>
-          </div>
+          </a>
 
-          <div className="function-item">
+          <a href="regulation.html" className="function-item">
             <img src="/icons/Regulation.png" alt="Thay đổi quy định" />
             <p>THAY ĐỔI QUY ĐỊNH</p>
-          </div>
+          </a>
 
         </div>
       </main>
